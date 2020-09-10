8dd4f50343672a4ee58af3fa031cde7ce0089115
torkhan <cedlec@wanadoo.fr>
2020-09-10 10:35:46 +0200

    10-09-20
    Changement mineurs
    tailles des cards à revoir

---
+++ Sources/main.js
@@ -0,0 +1,21 @@
+
+/*$(document).ready(function() {
+    let t = 200;
+    $(".card").hide();
+    $('.card').each( function(i){
+        let $this = $(this);
+
+        setTimeout(function() {
+            $this.show().animate({
+                opacity: '0.5',
+                height: '50%',
+                width: '50%'},1000);
+
+        }, t);
+
+        t +=650;
+
+    });
+});*/
+
+
